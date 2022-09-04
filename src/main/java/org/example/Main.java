40292a75cc1adef40a3cd50f32bc834dd5568eac
kkh1204 <21900063@handong.ac.kr>
2022-09-05 00:06:41 +0900

    ver 0.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
+/*class 와 interface를 많이 활용
+* C R U D 구현하기 interface는 확장가능성을 생각해서 만든 것.
+* 인터페이스 -> ICRUD*
+* 클래스 -> Word WordCRUD(인터페이스에서 상속) WordManager
+* 출력 -> [난이도|단어|단어의 뜻]*/
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
